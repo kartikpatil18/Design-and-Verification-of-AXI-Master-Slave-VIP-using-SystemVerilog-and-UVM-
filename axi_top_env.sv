d06acf2740b71d71b750398c6ec28dd1698fdb19
Kartik Ramesh Patil <142490220+kartikpatil18@users.noreply.github.com>
2026-05-25 22:52:29 +0530

    Update axi_top_env.sv

--- axi_top_env.sv
+++ axi_top_env.sv
@@ -1,23 +1,29 @@
 class axi_top_env extends uvm_env;
 	`uvm_component_utils(axi_top_env)
+	
 	function new(string name="", uvm_component parent=null);
 		super.new(name,parent);
 	endfunction 
+	
 	//master agent 
 	axi_master_agent master_agent;
 	//slave agent 
 	axi_slave_agent slave_agent;
 	//scoreboard 
 	axi_scoreboard    axi_sco;
+
+	//Build phase
 	function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
 		master_agent=axi_master_agent::type_id::create("master_agent",this);
 		slave_agent=axi_slave_agent::type_id::create("slave_agent",this);
 		axi_sco=axi_scoreboard::type_id::create("axi_sco",this);
 	endfunction
-
-       function void connect_phase(uvm_phase phase);
-           super.connect_phase(phase);
+	
+    //connect phase
+    function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
 	    master_agent.axi_m_mon.tlm_mon.connect(axi_sco.tlm_scor.analysis_export);
-       endfunction 	   
+    endfunction 
+	
 endclass
