5978742e87111fbdabbdc0aabbd0fbdb455f678b
Kartik Ramesh Patil <142490220+kartikpatil18@users.noreply.github.com>
2026-05-25 19:07:03 +0530

    Update axi_interface.sv

--- axi_interface.sv
+++ axi_interface.sv
@@ -1,4 +1,5 @@
 interface axi_interface(input bit aclk, aresetn);//clocking blocks and modports needed
+	
 	//5 channels 
 	//write address channel 10 signals 
 	logic awvalid;
@@ -11,19 +12,22 @@ interface axi_interface(input bit aclk, aresetn);//clocking blocks and modports 
 	logic [2:0] awprot;
 	logic [3:0] awlen;
 	logic [3:0] awcache;
+	
 	//write data channel 
 	logic [63:0] wdata;//maximum suppoprts 1024 bits of data 
 	logic [7:0] wstrb;// wdata data 8bytes are presnt so wstrb 8bit needed
-        logic wlast;
-        logic [15:0] wid;
-        logic wvalid;
-        logic wready;
-        //write response channel 
+    logic wlast;
+    logic [15:0] wid;
+    logic wvalid;
+    logic wready;
+    
+	//write response channel 
 	logic bvalid;
-        logic bready;
-        logic [15:0] bid;
-	logic[1:0] bresp;//ok.ex_ok, slave error , decode error   each transaction (number of beats) will get 1 response
-        //read address channel 10 signals 
+    logic bready;
+    logic [15:0] bid;
+	logic[1:0] bresp;//ok.ex_ok, slave error , decode error
+	
+    //read address channel 10 signals 
 	logic arvalid;
 	logic arready;
 	logic [31:0] araddr;
@@ -33,7 +37,8 @@ interface axi_interface(input bit aclk, aresetn);//clocking blocks and modports 
 	logic [2:0] arsize;
 	logic [2:0] arprot;
 	logic [3:0] arlen;
-	logic [3:0] arcache;	
+	logic [3:0] arcache;
+	
 	//read data channe;
 	logic rvalid;
 	logic rready;
@@ -41,4 +46,5 @@ interface axi_interface(input bit aclk, aresetn);//clocking blocks and modports 
 	logic rlast;
 	logic [1:0] rresp;//every beats will get response 
 	logic [63:0] rdata;
+	
 endinterface 
